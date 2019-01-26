4ed701243c6bb28b67874f6af4b954eaed3ef437
Richard Duivenvoorde <richard@duif.net>
2019-01-26 16:48:18 +0100

    Adding gpl header

--- QgisNetworkLogger.py
+++ QgisNetworkLogger.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
-# Import the PyQt and QGIS libraries
+# -----------------------------------------------------------
+# Copyright (C) 2019 Richard Duivenvoorde, Nyall Dawson
+# -----------------------------------------------------------
+# Licensed under the terms of GNU GPL 2
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# ---------------------------------------------------------------------
+
 from qgis.core import (
     QgsNetworkAccessManager,
     QgsNetworkReplyContent,
